df52bca889a2d4f16d0e398dd67b377efddaf1ce
funderos <benjamin-funder@hotmail.com>
2021-08-15 22:51:15 +0200

    Evaluation Update

--- website/views.py
+++ website/views.py
@@ -6,12 +6,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 import json
 import os
+import uuid
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-#@login_required
 def home():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -29,6 +29,35 @@ def home():
             flash('Username does not exist.', category='error')
 
     return render_template("home.html", user=current_user)
+
+
+@views.route('/starteval')
+def start_eval():
+    user = User(username=str(uuid.uuid4()), isRegistered=False, password=generate_password_hash(str(uuid.uuid4()), method='sha256'))
+    db.session.add(user)
+    db.session.commit()
+    flash('Evaluation started successfully!', category='success')
+    login_user(user, remember=True)
+    return redirect(url_for('views.home'))
+
+@views.route('/finisheval', methods=['GET', 'POST'])
+def finish_eval():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.isRegistered:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Username does not exist.', category='error')
+
+    return render_template("finisheval.html", user=current_user)
 
 @views.route('/favicon.ico') 
 def favicon():
